c9c0a312080872c7f4c529306dd6ee9edeb9a085
Parker <88950699+ParkerLockhart@users.noreply.github.com>
2021-12-01 16:55:00 -0600

    artist index and show

--- spec/features/artists/show_spec.rb
+++ spec/features/artists/show_spec.rb
@@ -1,17 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe 'the songs show page' do
-  it 'displays song title' do
+RSpec.describe 'the artists show page' do
+  it 'displays artists name' do
     artist = Artist.create!(name: 'Carly Rae Jepsen')
+    artist2 = Artist.create!(name: 'Prince')
     song = artist.songs.create!(title: "I Really Like You", length: 208, play_count: 234)
     song2 = artist.songs.create!(title: "Call Me Maybe", length: 207, play_count: 649)
-    visit "/songs/#{song.id}"
+    visit "/artists/#{artist.id}"
 
-    expect(page).to have_content(song.title)
-    expect(page).to_not have_content(song2.title)
+    expect(page).to have_content(artist.name)
+    expect(page).to_not have_content(artist2.name)
   end
 
-  it 'displays artist name for song' do
+  xit 'displays artist name for song' do
     artist = Artist.create!(name: 'Carly Rae Jepsen')
     song = artist.songs.create!(title: "I Really Like You", length: 208, play_count: 234)
     song2 = artist.songs.create!(title: "Call Me Maybe", length: 207, play_count: 649)
